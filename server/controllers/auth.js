8b4e5c68facc0fcd3f9201e9e338c5b36fcde51b
adeeb1488 <adeeb08ahmed@gmail.com>
2023-02-09 21:41:33 -0500

    Login API implementation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 import UserDetails from "../models/user.js"
 import bcrypt from 'bcryptjs'
+import { createError } from "../utils/errors.js"
 
 export const signup = async(req,res,next) =>{
     try{
@@ -10,8 +11,32 @@ export const signup = async(req,res,next) =>{
             email:req.body.email,
             password:hash,
         })
+        console.log(newUser)
         await newUser.save()
         res.status(201).send("User created successfully!")
+    }
+    catch(e){
+        next(e)
+    }
+}
+export const signin = async(req,res,next) =>{
+    try{
+      const user = await UserDetails.findOne({
+        user_name:req.body.user_name
+      })
+      console.log(user)
+      if(!user)
+      {
+        return next(createError(404,"User not found. Please try again..."))
+      }
+      const passwordValid = await bcrypt.compare(req.body.password, user.password)
+      if(!passwordValid) 
+      {
+        return next(createError(400,"Incorrect Password. Please try again..."))
+      }
+      
+        
+      res.status(200).json(user)
     }
     catch(e){
         next(e)
