20a098195a4ecb610802548bb4c4463fa3da4db6
adeeb1488 <adeeb08ahmed@gmail.com>
2023-03-11 12:41:35 -0500

    Creating search parameters in header, adding calendar component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import './Header.css'
+import { useState } from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBed, faCar, faPlane, faTaxi} from '@fortawesome/free-solid-svg-icons'
+import {faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi} from '@fortawesome/free-solid-svg-icons'
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css';
+import {format} from 'date-fns' // theme css file
 const Header = () => {
+  const[openCalendar, setOpenCalendar]= useState(false)
+  
+  const [date, setDate]= useState([
+    {
+      startDate: new Date(),
+      endDate:new Date(),
+      key:'selection'
+    }
+  ])
   return (
     <div className='header'>
       <div className="headerContainer">
@@ -31,7 +45,34 @@ const Header = () => {
       Book a hotel for your stay today and get 10% cashback and savings. 
      </p>
      <button className="headerBtn">Sign in / Register</button>
+     
+     <div className="headerSearch">
+     <div className="headerSearchItem">
+     <FontAwesomeIcon icon={faBed} className="headerIcon" />
+     <input type="text" placeholder='Where are you going?' className='headerSearchInput' />
      </div>
+     <div className="headerSearchItem">
+     <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"  />
+     <span onClick={()=>setOpenCalendar(!openCalendar)} className='headerSearchText'>{`${format(date[0].startDate,"MM/dd/yyyy")} to ${format(date[0].endDate,"MM/dd/yyyy")}`}</span>
+     {openCalendar&&<DateRange
+     editableDateInputs={true}
+     onChange={item=> setDate([item.selection])}
+     moveRangeOnFirstSelection={false}
+     ranges={date}
+     className="date"
+     
+     />}
+     </div>
+     <div className="headerSearchItem">
+     <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+     <span className='headerSearchText'>1 person 1 room</span>
+     </div>
+     <div className="headerSearchItem">
+    <button className="headerBtn">Search</button>
+     </div>
+     </div>
+     </div>
+
       </div>
   )
 }
