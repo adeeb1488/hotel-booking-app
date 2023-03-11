ea6d4c57fcf0d062e4e0c9565afe1f2de074e033
adeeb1488 <adeeb08ahmed@gmail.com>
2023-03-11 00:00:48 -0500

    Creating basic UI design for home page header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import './Header.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBed, faCar, faPlane, faTaxi} from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+      <div className="headerContainer">
+     <div className="headerList">
+      <div className="headerListItem active">
+      <FontAwesomeIcon icon={faBed} />
+      <span>Stays</span>
+      </div>
+      <div className="headerListItem">
+      <FontAwesomeIcon icon={faPlane} />
+      <span>Flights</span>
+      </div>
+      <div className="headerListItem">
+      <FontAwesomeIcon icon={faCar} />
+      <span>Car rental</span>
+      </div>
+      <div className="headerListItem">
+      <FontAwesomeIcon icon={faTaxi} />
+      <span>Taxi</span>
+      </div>
+     </div>
+     <h1 className="headerTitle">
+      Hotels at affordable prices? Why not?
+     </h1>
+     <p className="headerDescription">
+      Book a hotel for your stay today and get 10% cashback and savings. 
+     </p>
+     <button className="headerBtn">Sign in / Register</button>
+     </div>
+      </div>
   )
 }
 
